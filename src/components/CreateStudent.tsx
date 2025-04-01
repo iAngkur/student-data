9b9de1e6cd329f1b280d0ace38c2342c6a5d2862
Angkur Mondal <mondal.angkur@gmail.com>
2025-04-02 00:22:34 +0600

    feat(CreateStudent): enhance form submission with validation and state management
    - added form submission with proper validations
    - navigate to home after saving new student entry
    - linked all the input fields with respective states

--- src/components/CreateStudent.tsx
+++ src/components/CreateStudent.tsx
@@ -1,19 +1,68 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { FormEventHandler, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import {
+  showErrorToast,
+  showSuccessToast,
+  showWarningToast,
+} from "../utils/toast";
 
 export default function CreateStudent() {
+  const [id, setId] = useState("");
+  const [name, setName] = useState("");
+  const [place, setPlace] = useState("");
+  const [phone, setPhone] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleOnSubmit: FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+
+    if (!id || !name || !place || !phone) {
+      showWarningToast("Please fill all fields!");
+      return;
+    }
+
+    const newStudent = { id, name, place, phone };
+
+    fetch("http://localhost:9000/students", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newStudent),
+    })
+      .then((res) => {
+        if (!res.ok) throw new Error("Failed to save new student entry!");
+
+        showSuccessToast("Data saved successfully!");
+
+        setId("");
+        setName("");
+        setPlace("");
+        setPhone("");
+
+        navigate("/");
+      })
+      .catch((err) => showErrorToast(err.message));
+  };
+
   return (
     <div className="w-xl lg:w-3xl shadow-md mt-5 mx-auto p-5 border-t-5 border-purple-800 rounded-xl">
       <h2 className="text-center mb-5 text-purple-800 text-2xl font-bold">
         Add New Student
       </h2>
-      <form className="*:w-full [&>input]:mb-4">
+      <form
+        onSubmit={handleOnSubmit}
+        className="w-lg md:w-sm mx-auto *:w-full [&>input]:mb-4 [&>input]:px-2 [&>input]:py-1 font-semibold [&>input]:text-gray-600 [&>input]:text-sm"
+      >
         <label htmlFor="id">ID</label>
         <input
           type="text"
           id="id"
           name="id"
           className="border border-gray-200"
+          value={id}
+          onChange={(e) => setId(e.target.value)}
         />
         <label htmlFor="name">Name</label>
         <input
@@ -21,6 +70,8 @@ export default function CreateStudent() {
           id="name"
           name="name"
           className="border border-gray-200"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
         />
         <label htmlFor="place">Place</label>
         <input
@@ -28,6 +79,8 @@ export default function CreateStudent() {
           id="place"
           name="place"
           className="border border-gray-200"
+          value={place}
+          onChange={(e) => setPlace(e.target.value)}
         />
         <label htmlFor="phone">Phone</label>
         <input
@@ -35,12 +88,16 @@ export default function CreateStudent() {
           id="phone"
           name="phone"
           className="border border-gray-200"
+          value={phone}
+          onChange={(e) => setPhone(e.target.value)}
         />
         <div
           className="mt-5 flex justify-center *:px-4 *:py-1 gap-4 text-white *:rounded  *:cursor-pointer *:hover:scale-105 *:duration-300
         *:ease-in"
         >
-          <button className="bg-purple-800">Save</button>
+          <button type="submit" className="bg-purple-800">
+            Save
+          </button>
           <Link to="/" className="bg-red-800">
             Back
           </Link>
