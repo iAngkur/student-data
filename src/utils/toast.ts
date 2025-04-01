ff8852debe162e4d3e696cd3826f2b3e04f4fc77
Angkur Mondal <mondal.angkur@gmail.com>
2025-04-02 00:02:50 +0600

    feat(toast): add unique toastId to each toast message for better identification
    - stopped showing a stack of toats of the same error message at a time
    - added "message" as a unique id for each type of toast

--- src/utils/toast.ts
+++ src/utils/toast.ts
@@ -7,20 +7,21 @@ const commonOptions = {
   pauseOnHover: true,
   draggable: true,
   progress: undefined,
+  toastId: "",
 };
 
 export const showToast = {
   success: (message: string, autoClose: number = 3000) => {
-    toast.success(message, { ...commonOptions, autoClose });
+    toast.success(message, { ...commonOptions, autoClose, toastId: message });
   },
   error: (message: string, autoClose: number = 4000) => {
-    toast.error(message, { ...commonOptions, autoClose });
+    toast.error(message, { ...commonOptions, autoClose, toastId: message });
   },
   warning: (message: string, autoClose: number = 3500) => {
-    toast.warning(message, { ...commonOptions, autoClose });
+    toast.warning(message, { ...commonOptions, autoClose, toastId: message });
   },
   info: (message: string, autoClose: number = 2500) => {
-    toast.info(message, { ...commonOptions, autoClose });
+    toast.info(message, { ...commonOptions, autoClose, toastId: message });
   },
 };
 
